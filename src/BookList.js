b0c847301a4e694a9f80d7d2cca779abf7829faf
Brandon Lothaire Atwell <batwell31@yahoo.com>
2018-08-15 17:48:05 -0400

    add code to BookList component to application

---
+++ src/BookList.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Book from "./Book";
+
+class BookList extends React.Component {
+    render() {
+        let shelfNames = ["Currently Reading", "Want to Read", "Read", "None"];
+        let shelfTypes = ["currentlyReading", "wantToRead", "read", "none"];
+
+        const shelf = shelfTypes.map(
+            (x, i) => {
+                let books = (this.props.books != undefined && this.props.books instanceof Array) && (this.props.books.filter((books) => books.shelf === x))
+                return (
+                    <div key={x} className="book-shelf-detail">
+                        <p> Bookshelf Name : {shelfNames[i]}</p>
+                        <ol className="books-grid">
+                            {books != undefined && books.length !== 0 &&
+                                books.map((book, index) => {
+                                    return (
+                                        <li key={index}>
+                                            <Book book={book} />
+                                        </li>
+                                    );
+                                })}
+                        </ol>
+                    </div>
+                )
+            }
+        );
+
+        return (
+            <div className="book-list">
+                {shelf}
+            </div>
+        )
+    }
+}
+
+export default BookList
