2c38beb36974755cb491ebb6e96c4bc72dd0ba50
Brandon Lothaire Atwell <batwell31@yahoo.com>
2018-08-15 19:37:49 -0400

    re-write SearchBook component due to previous code bugging out.

--- src/SearchBook.js
+++ src/SearchBook.js
@@ -1,33 +1,71 @@
-import React from 'react'
+import React, { Component } from 'react';
 import { Link } from "react-router-dom"
+import PropTypes from 'prop-types';
 import BookList from "./BookList"
+import * as BooksAPI from './BooksAPI';
 
-class SearchBook extends React.Component {
+class SearchBook extends Component {
+    static propTypes = {
+        onChangeShelf: PropTypes.func.isRequired
+    }
+
+    state = {
+        query: '',
+        searchedBooks: []
+    }
+
+    updateQuery = (query) => {
+        this.setState({
+            query: query.trim()
+        });
+        this.searchBooks(query);
+    }
+
+    searchBooks = (query, maxResults) => {
+        BooksAPI.search(query, maxResults || 10)
+            .then(searchedBooks => {
+                if (!searchedBooks || searchedBooks.error)
+                    return [];
+
+                let response = [];
+
+                for (const searchedBook of searchedBooks) {
+                    for (const book of this.props.books) {
+                        if (searchedBook.id === book.id) {
+                            console.log('searched book %j', searchedBook);
+                            searchedBook.shelf = book.shelf;
+                        }
+                    }
+                    response.push(searchedBook);
+                }
+
+                return response;
+            })
+            .then(searchedBooks => {
+                this.setState(oldState => ({ searchedBooks }))
+            })
+            .catch(err => console.error('Error occurred searching books: ', err));
+    }
+
     render() {
-        return(
+        const { onChangeShelf } = this.props;
+        const { query, searchedBooks } = this.state;
+
+        return (
             <div className="search-books">
                 <div className="search-books-bar">
-                    <Link className="close-search-book" to="/">
-                        Close
-                </Link>
-                    <div className="search-book-input">
-                        <input
-                            type="text"
-                            placeholder="Search books by title or author"
-                            onChange={(event) => this.props.onBookSearch(event.target.value)}
+                    <Link className="close-search" to="/">Close</Link>
+                    <div className="search-books-input-wrapper">
+                        <input type="text" placeholder="Search by title or author" value={query}
+                            onChange={(event) => this.updateQuery(event.target.value)}
                         />
                     </div>
                 </div>
-                <div className="search-book-result">
-                    <ol className="books-grid">
-                        <BookList
-                            books={this.props.books}
-                            onMoveBook={this.props.onChangeShelf}
-                        />
-                    </ol>
+                <div className="search-books-results">
+                    <BookList books={searchedBooks} onChangeShelf={onChangeShelf} />
                 </div>
-            </div> 
-        )   
+            </div>
+        )
     }
 }
 
