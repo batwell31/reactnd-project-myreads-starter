416ce03d4b66591a5accbb7eb6dc7eb9e6a23864
Brandon Lothaire Atwell <batwell31@yahoo.com>
2018-08-16 16:20:25 -0400

    check to see if thumbnail is null in BookList component

--- src/BookList.js
+++ src/BookList.js
@@ -22,7 +22,7 @@ class BookList extends Component {
                             <li key={book.id}>
                                 <div className="book">
                                     <div className="book-top">
-                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks === undefined ? null : book.imageLinks.thumbnail})` }}></div>
                                         <div className="book-shelf-changer">
                                             <select value={book.shelf} onChange={this.handleMove.bind(this, book)}>
                                                 <option value="none">Move to...</option>
